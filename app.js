1e40ba1f342c5d6824249a1982ec3714d4c6cafc
Tal <talsch22@gmail.com>
2020-11-04 14:10:44 +0200

    Move to google cloud datastore

--- app.js
+++ app.js
@@ -6,90 +6,147 @@ const express = require('express');
 const app = express();
 
 
-//DISK DATABASE
-const sqlite3 = require('sqlite3').verbose();
-// create a new database file users.db or open existing users.db
-const ip_db = new sqlite3.Database('./Database/ip.db', (err) => {
-  if (err) {
-      console.error(err.message);
-  }
-  console.log('Connected to the ip.db database.');
-});
+/*
+// Imports the Google Cloud client library.
+const {Storage} = require('@google-cloud/storage');
 
-ip_db.run('CREATE TABLE IF NOT EXISTS ip(ip_address text)', (err) => {
-  if (err) {
-    console.log(err);
-    throw err;
-  }
-});
+// Instantiates a client. If you don't specify credentials when constructing
+// the client, the client library will look for credentials in the
+// environment.
+const storage = new Storage();
+// Makes an authenticated API request.
+async function listBuckets() {
+  try {
+    const results = await storage.getBuckets();
 
-app.get('/setip/:ip', (req, res) => {
+    const [buckets] = results;
+
+    console.log('Buckets:');
+    buckets.forEach(bucket => {
+      console.log(bucket.name);
+    });
+  } catch (err) {
+    console.error('ERROR:', err);
+  }
+}
+listBuckets();
+*/
+
+///////////// DATASTORE ////////////////////
+//const crypto = require('crypto');
+app.enable('trust proxy');
+// By default, the client will authenticate using the service account file
+// specified by the GOOGLE_APPLICATION_CREDENTIALS environment variable and use
+// the project specified by the GOOGLE_CLOUD_PROJECT environment variable. See
+// https://github.com/GoogleCloudPlatform/google-cloud-node/blob/master/docs/authentication.md
+// These environment variables are set automatically on Google App Engine
+const {Datastore} = require('@google-cloud/datastore');
+// Instantiate a datastore client
+const datastore = new Datastore();
+
+
+async function deleteIpObjects(){
+  let query = datastore.createQuery('ip').limit(10);
+  const [ress] =  await  datastore.runQuery(query);
+  if(ress.length >0){
+    const keys = ress.map(r=>{return r[datastore.KEY]});
+    const transaction = datastore.transaction();
+    await transaction.run(function(err) {
+      if (err) {}
+      else{
+        // Delete a entities.
+        transaction.delete(keys);
+        transaction.commit(function(err) {
+          if (!err) {}
+        });
+      }
+    });
+  }
+}
+
+async function setIP(ip){
+  await deleteIpObjects();
+	//save to datatore
+	return datastore.save({
+		key: datastore.key('ip'),
+		data: {ip_address:ip},
+  });
+  
+}
+
+app.get('/setip/:ip', async (req, res) => {
   if(!req.params.ip){
     res.status(404).send("please specify ip").end();
     return;
   }
   const ip = req.params.ip;
-  ip_db.serialize(() => {
-    ip_db.run('DELETE FROM ip');
-    ip_db.run('INSERT INTO ip(ip_address) VALUES(?)', [ip],function(err) { 
-			if (err) {							
-				console.log("error during insert to inventory:"+err);							
-			}
-		});
-  });
-
-  
-  res.status(200).send('ip set : '+ip).end();
+  try{
+    await setIP(ip);
+    res.status(200).send('ip set : '+ip).end();
+  }catch(err){
+    res.status(200).send('error : '+err).end();
+  }
 
 });
 
-async function getIP(){
-  return new Promise(resolve => {
-		try{
-			ip_db.all(`SELECT * FROM ip`, (err, rows) => {
-			  if (err) {
-          resolve({ok:false, data:err});
-          return;
-        }
-        if(rows) {
-          if( rows.length == 0){
-            resolve( {ok:true, data:'no ip set'});
-          }else{
-            resolve( {ok:true, data:rows[0].ip_address});
-          }
-        
-        }else {
-          resolve( {ok:true, data:'no ip set'});
-        }
-			  });
-		  } catch(error){
-			 resolve( {ok:false, data:error})
-		  }
-	});
+async function getAllIP(){
+  let query = datastore.createQuery('ip').limit(10);
+  const [ress] =  await  datastore.runQuery(query);
+  return ress;
 }
 
-app.get('/getip', (req, res) => {
+async function getIP(){
+  let query = datastore.createQuery('ip').limit(10);
+  const [ress] =  await  datastore.runQuery(query);
+  if(ress.length==0){
+    return "empty";
+  }else{
+    return (ress[ress.length -1]).ip_address;
+  }
+	//const obj = ress[0];
+	//return obj.ip_address;
+}
 
-  getIP().then(result=>{
-    if(result.ok){
-      res.status(200).send('ip set : '+result.data).end();
-    }else{
-      res.status(200).send('error : '+result.data).end();
-    }
-  });
-
+app.get('/getip', async (req, res) => {
+  try{
+    const ip= await getIP();
+    res.status(200).send('ip is : '+ ip).end();
+  }catch(err){
+    res.status(200).send('err : '+ err).end();
+  }
 });
 
-app.get('/', (req, res) => {
-  getIP().then(result=>{
-    if(result.ok){
-      res.status(200).send('<html><script>window.location.href="http://' +result.data+ '"</script></html>');
-    }else{
-      res.status(200).send('error : '+result.data).end();
-    }
-  });
+app.get('/', async (req, res) => {
+  try{
+    const ip= await getIP();
+    res.status(200).send('<html><script>window.location.href="http://' +ip+ '"</script></html>');
+  }catch(err){
+    res.status(200).send('err : '+ err).end();
+  }
 });
 
+app.get('/list', async (req, res) => {
+  try{
+    const ips= await getAllIP();
+    res.json(ips).end();
+  }catch(err){
+    res.status(200).send('err : '+ err).end();
+  }
+});
+
+
+// Start the server
+const PORT = process.env.PORT || 80;
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+  console.log('Press Ctrl+C to quit.');
+});
+// [END gae_node_request_example]
+
+module.exports = app;
+
+
+/*
 app.get('/r1', (req, res) => {
   res.redirect('185.110.110.191/');
 });
@@ -110,18 +167,8 @@ app.get("/r5", (req, res) => {
       res.status(200).send('error : '+result.data).end();
     }
   });
-
- 
-
 })
 
 
-// Start the server
-const PORT = process.env.PORT || 80;
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}`);
-  console.log('Press Ctrl+C to quit.');
-});
-// [END gae_node_request_example]
 
-module.exports = app;
+*/
